aca0e79ac413d85fec3e36646b6b4a98176ce957
Jelena Mitrovic <jelenamitrovic980@gmail.com>
2020-07-21 13:03:25 +0200

    create project

---
+++ manyToManyDemo/src/main/java/com/jelenamitrovic980/manyToManyDemo/ManyToManyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jelenamitrovic980.manyToManyDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyToManyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManyToManyDemoApplication.class, args);
+	}
+
+}
