75c995a4863a48cbe0a48b0f41e566301d766aa6
Jelena Mitrovic <jelenamitrovic980@gmail.com>
2020-07-21 20:56:25 +0200

    add class

---
+++ manyToManyDemo/src/main/java/com/jelenamitrovic980/manyToManyDemo/Book.java
@@ -0,0 +1,53 @@
+package com.jelenamitrovic980.manyToManyDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int bookId;
+	private String bookName;
+	
+	@ManyToMany
+	@JoinTable(name = "booksAuthors", joinColumns = @JoinColumn(name = "bookId"),
+	inverseJoinColumns = @JoinColumn(name = "authorId"))
+	private List<Author> authors = new ArrayList<>();
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public List<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<Author> authors) {
+		this.authors = authors;
+	}
+	
+	
+
+}
